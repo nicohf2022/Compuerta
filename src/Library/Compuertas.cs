9a13b1dda6e3dbb3119a1a73b49d18f8a32591a3
nicohf2022 <104432397+nicohf2022@users.noreply.github.com>
2022-12-13 00:28:44 -0300

    ejercicio con comentarios

---
+++ src/Library/Compuertas.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+
+
+
+
+
+
+namespace Library;
+
+/// <summary>
+/// se crea la clase abstracta  Compuertas que implementa la interfaz IInput
+/// </summary>
+public abstract class  Compuertas: IInput
+
+{   
+   /// <summary>
+   ///  se crea el constructor de la clase Compuertas
+   /// </summary>
+   /// <param name="name"></param>
+    public Compuertas(string name)
+    {
+        this.Name = name;
+        this.Input = new Dictionary<string, IInput>(); //se crea un diccionario de entradas
+
+    }
+      
+      /// <summary>
+      /// se crea la propiedad Name
+      /// </summary>
+      /// <value></value>
+      public string Name { get; set; }
+      /// <summary>
+      ///  se crea un diccionario de entradas
+      /// </summary>
+      /// <value></value>
+      
+      public Dictionary<string, IInput> Input { get; set; }
+
+
+    
+
+    /// <summary>
+    /// se crea el metodo AddInput que agrega una entrada al diccionario
+    /// </summary>
+    public void AddInput(string name, IInput value)
+    {
+        if (Input.ContainsKey(name))
+        {
+            Input[name] = value;
+        }
+        else
+        {
+            Input.Add(name, value);
+        }
+    }         
+    
+/// <summary>
+///  se crea el metodo result que retorna el resultado de la compuerta
+/// </summary>
+/// <returns></returns>
+public abstract int result();
+
+}       
+
+
+
+
+    
+    
---
+++ src/Library/LogicInput.cs
@@ -0,0 +1,29 @@
+namespace Library;
+/// <summary>
+///  se crea la clase LogicInput que implementa la interfaz Iinput
+/// </summary>
+public class LogicInput : IInput
+{
+   
+    /// <summary>
+    /// se crea el constructor de la clase LogicInput
+    /// </summary>
+    public LogicInput(int value)
+    {
+        this.Value= value;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Value { get; set; }
+
+    /// <summary>
+    /// se crea el metodo result que retorna el valor de la compuerta
+    /// </summary>
+    public int result()
+    {
+        return this.Value;
+    }
+
+}
